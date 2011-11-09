7b2935b6352b86f96de4b45335134fc090ff17c9
Huned Botee <huned@734m.com>
2011-11-09 12:55:36 -0800

    vanilla rails 3.1.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigscreen::Application.config.secret_token = 'b1ea74c5f2d1f2ef63c1013803a934b09e8c0793adc32f49c623a5d66aa15dc0d4e33ecb432a6f08327a0e195df5be055cc864d4e21fe02d3df46180d110ca7b'
